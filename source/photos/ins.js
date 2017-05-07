2c5a5dca85e685d4d404d2379725559dc3e15c61
coy <coy20150428@gmail.com>
2017-05-07 13:03:11 +0800

    添加了自动生成图片的python脚本文件

--- source/photos/ins.js
+++ source/photos/ins.js
@@ -116,11 +116,11 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'http://litten.me/ins-min/' + data.link[i] + '.min.jpg';
-          var src = 'http://litten.me/ins/' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/TheCoy/thecoy.github.com/deploy/min_photos/' + data.link[i];
+          var src = 'https://raw.githubusercontent.com/TheCoy/thecoy.github.com/deploy/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
-          src += '.jpg';
+          src += '';
 
           liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
                 <a href="' + src + '" itemprop="contentUrl" data-size="640x640" data-type="' + type + '" data-target="' + target + '">\
